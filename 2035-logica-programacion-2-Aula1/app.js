248c2f82c97a69eb4a6d229aa890276af8726434
coteando <116815021+coteando@users.noreply.github.com>
2025-01-29 18:39:54 +0000

    Add files via upload

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto'
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un número entre el 1 y el 10'; 
+
+function intentoDeUsuario() {
+    alert('click desde la funcion ')
+
+}
